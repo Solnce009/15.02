4e4e20e37d3a526a02ad67c9308d1776603c491c
Solnce009 <solnushkinigor@gmail.com>
2025-03-22 15:29:54 +0300

    add list

--- src/main/java/org/exapmle/edu/controller/StudentController.java
+++ src/main/java/org/exapmle/edu/controller/StudentController.java
@@ -1,6 +1,9 @@
 package org.exapmle.edu.controller;
 
 import org.exapmle.edu.model.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -9,4 +12,18 @@ import java.util.List;
 @RestController
 public class StudentController {
     private List<Student> studentList = new ArrayList<>();
+    private long countId = 0;
+
+    @GetMapping("/student")
+    public List<Student> getStudentList(){
+        return  studentList;
+    }
+
+    @PostMapping("/student")
+    public long saveStudent(@RequestParam String name, @RequestParam String email, @RequestParam int age) {
+        countId = countId + 1;
+        Student savedStudent = new Student(countId, name, email, age);
+        studentList.add(savedStudent);
+        return savedStudent.getId();
+    }
 }
