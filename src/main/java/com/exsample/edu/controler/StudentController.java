295ca828a70d44a06ee6f173e8f28c7e1b64a397
Solnce009 <solnushkinigor@gmail.com>
2025-04-05 13:39:49 +0300

    table

--- src/main/java/com/exsample/edu/controler/StudentController.java
+++ src/main/java/com/exsample/edu/controler/StudentController.java
@@ -2,29 +2,25 @@ package com.exsample.edu.controler;
 
 import com.exsample.edu.model.Student;
 import com.exsample.edu.repository.StudentRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class StudentController {
     private final StudentRepository repository;
-    private long countId = 0;
-
-    public StudentController(StudentRepository repository) {
-        this.repository = repository;
-    }
 
     @GetMapping("/student")
     public List<Student> getStudentList() {
-        return repository.getStudentList();
+        return repository.findAll();
     }
 
     @PostMapping("/student")
     public long saveStudent(@RequestParam String name, @RequestParam String email, @RequestParam int age) {
-        countId = countId + 1;
-        Student savedStudent = new Student(countId, name, email, age);
-        return  repository.save(savedStudent);
+        Student savedStudent = new Student(name, email, age);
+        return  repository.save(savedStudent).getId();
     }
 
     @GetMapping("/student/{id}")
